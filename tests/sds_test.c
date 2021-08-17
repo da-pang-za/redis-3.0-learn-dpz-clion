0848dcae6522816aa10a3c0130e3f37aa9a49b46
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2021-08-18 00:14:02 +0800

    增加测试

--- tests/sds_test.c
+++ tests/sds_test.c
@@ -3,13 +3,35 @@
 #include <string.h>
 #include "sds.h"
 
+//sds内存预分配
+void testSDSRoomAlloc() {
+    sds s = sdscat(sdsempty(), "Redis");
+
+    printf("s: %s\n", s);
+
+    sdscat(s, " Cluster");
+    printf("s: %s\n", s);
+    printf("avail length:%d", sdsavail(s));//13
+
+}
+//c语言字符串
+void testCstr() {
+    sds s = sdscat(sdsempty(), "Redis");
+    s[6]='@';
+    for (int i = 0; i < 10; ++i) {
+        printf("s: %c\n", s[i]);
+
+    }
+    printf("s: %s\n", s);
+
+
+}
+
 int main(void) {
 
-    sds empty = sdsempty();
-    sdscat(empty,"dpz");
-    sdscat(empty,"-hpf");
-    size_t i = sdsavail(empty);
-    printf("avail length:%d", i);
+
+    testCstr();
     return 0;
 
 }
+

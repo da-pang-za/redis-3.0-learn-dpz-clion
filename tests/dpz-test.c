cfac5925b49b65ed598d6075f446bd76cc8a7b28
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2021-09-11 15:59:02 +0800

    c语言 指针  printf

--- tests/dpz-test.c
+++ tests/dpz-test.c
@@ -38,6 +38,13 @@ void testPointer() {
     void *pointer1;
     int *pointer2;
     double *pointer3;
+    double k=2.2;
+    pointer3=&k;
+    printf("%lu\n",pointer3);//指针本身的值  即对象的地址
+
+    printf("%lf\n",*pointer3);//对象值
+    printf("%lf\n",k);
+
     printf("%lu\n", sizeof(pointer1));
     printf("%lu\n", sizeof(pointer2));
     printf("%lu\n", sizeof(pointer3));
